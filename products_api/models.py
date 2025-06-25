5c4e1632f5291e46042c681e3bd49deb237beff9
Saif Rahman <Saf.saif@gmail.com>
2025-06-25 11:56:23 +0600

    Created models

--- products_api/models.py
+++ products_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    """
+    Defiines the product Structure.
+    """
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+
+    # Timestamps for product creation and updates
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-created_at']
+    
