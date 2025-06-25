662fa0f6f23f6897ef40a83aec02f72fb771d21b
Saif Rahman <Saf.saif@gmail.com>
2025-06-25 15:47:55 +0600

    urls updated

--- products_api/urls.py
+++ products_api/urls.py
@@ -3,5 +3,7 @@ from .views import ProductViewSet
 
 # Creating a router instance and register our viewsets with it.
 router = DefaultRouter()
+
 router.register(r'products', ProductViewSet, basename='product')
+# The API URLs are now determined automatically by the router.
 urlpatterns = router.urls
