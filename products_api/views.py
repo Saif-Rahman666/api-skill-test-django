4a75c07a3cd1f47ac089b3d3a36391d91e0fa367
Saif Rahman <Saf.saif@gmail.com>
2025-06-25 12:13:20 +0600

    using ModelViewSet for crud

--- products_api/views.py
+++ products_api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    #Custom methods for create and update
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
